a0a6bfb2f5dda72ae23772fd86aba3fc353f4725
tspeu <tenshotseuropa@gmail.com>
2018-11-01 15:38:33 +0000

    add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
+
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { NgModule , CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule } from '@angular/material';
+import { MatFormFieldModule , MatInputModule , MatGridListModule} from '@angular/material';
+
 import { LoginComponent } from './login/login.component';
 import { VehicleformComponent } from './vehicleform/vehicleform.component';
 
@@ -15,9 +19,20 @@ import { VehicleformComponent } from './vehicleform/vehicleform.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule, MatGridListModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
--- src/app/vehicleform/vehicleform.component.ts
+++ src/app/vehicleform/vehicleform.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-vehicleform',
   templateUrl: './vehicleform.component.html',
   styleUrls: ['./vehicleform.component.css']
 })
+
+// vehicle = {
+//   ManufacturerNameShort: string;
+//   vehicleId: string;
+//   type: number;
+//   price: number;
+
+// }
+
 export class VehicleformComponent implements OnInit {
+
+  vehicleId: string;
+  type: number;
+  price: number;
+  manufacturer: string;
+
+  model: any = {};
+// reactive form
+  vehicleForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+                                          // form state
+    this.vehicleForm = new FormGroup({
+      vehicleIdInput: new FormControl('', Validators.required),
+      typeInput: new FormControl('',  Validators.required),
+      manufacturerInput: new FormControl(),
+      priceInput: new FormControl()
+    });
   }
+
+  sendData() {
+
+    // console.log($event.value);
+    // console.log( this.manufacturer);
+    // console.log( this.vehicleId);
+    // console.log( this.type);
+    // console.log( this.price);
+    console.log('formulario' + JSON.stringify(this.vehicleForm.value));
+  }
+
+  cancel() {
+    console.log('cancel');
+  }
+/*
+VehicleId int { get; set; }
+string Type { get; set; }
+string ManufacturerNameShort { get; set; }
+ decimal Price  */
 
 }
